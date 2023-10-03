a3f82d5c62820cc0b9fd633e7ad1bab0cb281aa0
njzhu <njzhu@andrew.cmu.edu>
2023-10-02 20:46:31 -0400

    checking

--- src/tt_um_njzhu_calculator.sv
+++ src/tt_um_njzhu_calculator.sv
@@ -18,7 +18,7 @@ module tt_um_njzhu_calculator (
     logic [3:0] inReg, outReg;
     logic [1:0] op;
     logic [3:0] a, b;
-    always_comb begin : 
+    always_comb begin
         case (op)
           2'b00: inReg =  a + b;
           2'b01: inReg = a - b;
